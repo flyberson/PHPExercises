6960610a7b968ecf15b8b8526660bdc70cab2cb9
flyberson <flyberson12@gmail.com>
2017-10-04 10:28:14 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<html
+<body
+<header><title>This is title</title>
+   </header>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: flyberson
+ * Date: 10/4/17
+ * Time: 9:15 AM
+ */
+
+echo "hello";
+
+for ($i=0;$i<=10;$i++){
+    echo $i;
+}
+
+
+?>
+<br>
+<?php
+for ($i=0;$i<=10;$i++){
+
+   // echo $i*$i;
+    for ($j=0;$j<=10;$j++){
+        echo $i."*".$j."=". $i*$j."<br>";
+    }
+
+}
+//http://localhost:63342/PHPExercises/index.php?name=friend
+// tilføj efter ? get variabler
+print_r($_GET);
+echo "<br> hello"." ".$_GET['name'];
+
+$username1 = "firstusername";
+$username2 = "secondusername";
+$password1 = "firstpasswrod";
+$password2 = "secondpassword";
+$array1 =array($userarray = array("username1","username2"), $passarray =array("password1","password2"));
+echo"<br>";
+echo $array1[0][0]."<br>";
+echo $array1[0][1] ."<br>";
+echo $array1[1][0]."<br>";
+echo $array1[1][1]."<br>";
+
+$inputuser = $_GET['username'];
+$inputpass = $_GET['password'];
+echo $inputuser;
+echo $inputpass;
+
+//for ($i=0;$i < $userarray)
+for ($i=0;$i<= count($userarray);$i++) {
+    if ($inputuser==$userarray[$i]){
+      if($inputpass==$passarray[$i]){
+          echo "logged in";
+
+      }
+    }
+
+}
+?>
+</body>
+</html>
